c5d883ca8a5a4a63765fabe9ab7fde4f93c080a6
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-05 15:58:51 +0100

    Created window, with close functionality

--- Main.cpp
+++ Main.cpp
@@ -8,14 +8,28 @@ int main()
 {
 	//variables
 	sf::RenderWindow gameWindow; //Makes a variable called gameWindow of the type renderwindow
-
+	gameWindow.create(sf::VideoMode::getDesktopMode(),"Button Masher",
+		sf::Style::Titlebar | sf::Style::Close);
 
 
 	//game loop
-	while (true) // while continues to loop while the (condition) is true
+	//Runs every frame untile the game is closed
+	while (gameWindow.isOpen()) // while continues to loop while the (condition) is true
 	{
-		//TODO: check for input
+		//Check for input
+		sf::Event gameEvent;
 
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			//Process events
+
+			//check if event is closed event
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				//close the window
+				gameWindow.close();
+			}
+		}
 
 		//TODO: update game state
 
