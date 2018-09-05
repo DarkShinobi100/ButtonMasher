94592c7305c47e145c09e01ec95b072f8efbe117
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-05 15:32:32 +0100

    setup SFML

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+
+//entry point for the game
+int main()
+{
+	//game loop
+	while (true) // while continues to loop while the (condition) is true
+	{
+		
+
+
+	}
+
+
+	//exit point for the game
+	return 0;
+}
