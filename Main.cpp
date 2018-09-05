d9688d39caaeb6f60cbfb3ec94450ff4fb3a46e2
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-05 15:38:34 +0100

    Initial copy with SFML, added main window

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,27 @@
+//included loibraries
+#include <SFML/Graphics.hpp>
+
+
 
 //entry point for the game
 int main()
 {
+	//variables
+	sf::RenderWindow gameWindow; //Makes a variable called gameWindow of the type renderwindow
+
+
+
 	//game loop
 	while (true) // while continues to loop while the (condition) is true
 	{
-		
+		//TODO: check for input
+
+
+		//TODO: update game state
+
+
+		//TODO:Draw Graphics
+
 
 
 	}
