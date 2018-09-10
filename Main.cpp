2184e65dbba18876a6f08da4937d5f2b4e1ba908
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-10 15:50:29 +0100

    Sprite now displaying in center of the screen

--- Main.cpp
+++ Main.cpp
@@ -11,8 +11,26 @@ int main()
 	gameWindow.create(sf::VideoMode::getDesktopMode(),"Button Masher",
 		sf::Style::Titlebar | sf::Style::Close);
 
+	//----------------------------------------------
+	//game setup
+	//----------------------------------------------
+	
+	//create button sprite
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
 
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+
+
+	//center sprite in the middle of the screen, you have to realign it as the anchor point is the top left
+	buttonSprite.setPosition(gameWindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+		gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2);
+
+
+	//----------------------------------------------
 	//game loop
+	//----------------------------------------------
 	//Runs every frame untile the game is closed
 	while (gameWindow.isOpen()) // while continues to loop while the (condition) is true
 	{
@@ -36,6 +54,14 @@ int main()
 
 		//TODO:Draw Graphics
 
+		//clear the window to a single colour
+		gameWindow.clear(sf::Color::Cyan);
+
+		// Draw everything
+		gameWindow.draw(buttonSprite);
+
+		//Display the window contents on the screen
+		gameWindow.display();
 
 
 	}
